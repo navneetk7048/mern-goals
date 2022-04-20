96c0c2b80339c5951bf99c2ee534d2d39389a8d2
Navneet Vishwakarma <navneetk7048@gmail.com>
2022-04-20 12:59:07 +0530

    Make the project deployable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import colors from "colors";
 import { config } from "dotenv";
+import path from "path";
 import goalRoutes from "./routes/goalRoutes.js";
 import { errorHandler } from "./middleware/errorMiddleware.js";
 import { connectDB } from "./config/db.js";
@@ -13,8 +14,12 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static("./client/build"));
 
 app.use("/api/goals", goalRoutes);
+app.get("/", (req, res) => {
+  res.status(200).send("./client/build/index.html");
+});
 
 app.use(errorHandler);
 
