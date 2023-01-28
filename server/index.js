a556b169fe55a1ed9788471c2d3be68ffdf46e40
Navneet Vishwakarma <navneetk7048@gmail.com>
2023-01-28 14:35:15 +0530

    Separate client and server

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import cors from "cors";
+import { config } from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import { errorHandler } from "./middleware/errorMiddleware.js";
+import goalRoutes from "./routes/goalRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
+config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/goals", goalRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(errorHandler);
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.error(err));
